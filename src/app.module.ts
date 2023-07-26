98009d10aeabe93f6ac134c39bc65df80fd6b533
Jesse Jonas <jessejonas13@gmail.com>
2023-07-26 17:14:47 -0300

    chore: added axios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppService } from './app.service';
 import { ListsModule } from './lists/lists.module';
+import { List } from './lists/entities/list.entity';
 
 @Module({
-  imports: [ListsModule],
+  imports: [
+    ListsModule,
+    SequelizeModule.forRoot({
+      autoLoadModels: true, // feature for dev
+      dialect: 'sqlite',
+      host: ':memory',
+      models: [List],
+    }),
+  ],
   controllers: [],
   providers: [AppService],
 })
--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,9 +1,18 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { ListsService } from './lists.service';
 import { ListsController } from './lists.controller';
+import { List } from './entities/list.entity';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      baseURL: 'http://localhost:8000',
+    }),
+    SequelizeModule.forFeature([List]),
+  ],
   controllers: [ListsController],
-  providers: [ListsService]
+  providers: [ListsService],
 })
 export class ListsModule {}
